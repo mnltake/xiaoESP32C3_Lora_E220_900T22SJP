c407fcceeb613e06d33a3cc2e913263543f3b594
mnltake <minolu2003@ybb.ne.jp>
2024-01-28 18:10:24 +0900

    WoR OK D0=M0 D1=M1 D2=AUX

--- src/main.cpp
+++ src/main.cpp
@@ -10,11 +10,11 @@
 #define I2C_DEV_ADDR 0x55
 
 // E220-900T22S(JP)へのピンアサイン
-#define LoRa_ModeSettingPin_M0 D7
-#define LoRa_ModeSettingPin_M1 D7
+#define LoRa_ModeSettingPin_M0 GPIO_NUM_2//D0 =GPIO2
+#define LoRa_ModeSettingPin_M1 GPIO_NUM_3//D1 =GPIO3
 #define LoRa_Rx_ESP_TxPin D8
 #define LoRa_Tx_ESP_RxPin D9
-#define LoRa_AUXPin D10
+#define LoRa_AUXPin GPIO_NUM_4//D2
 
 // E220-900T22S(JP)のbaud rate
 #define LoRa_BaudRate 9600
@@ -23,21 +23,21 @@ int16_t senserID = -1;
 #define OWN_ADDRESS 131
 // #define SECOND_ADDRESS 305
 
-#define SW_LOW D0
-#define SW_HIGH D1
-#define SW_COM D2
+#define SW_LOW D4
+#define SW_HIGH D5
+#define SW_COM D6
 // #define I2C_VCC D3
-#define I2C_SDA D4
-#define I2C_SCL D5
+// #define I2C_SDA D4
+// #define I2C_SCL D5
 #define SECOND_SW_LOW D3
-#define SECOND_SW_HIGH D6
+// #define SECOND_SW_HIGH D6
 // #define SECOND_SW_COM D5
 // #define SENSOR_GND D5
 // #define SENSOR_3V3 D6
 // #define SENSOR_DQ D3
 uint64_t sleepSec = 60*60 - 5;//実行時間5ｓ
 RTC_DATA_ATTR uint16_t bootCount = 0;
-
+esp_sleep_source_t  wakeup_reason;
 //WDT
 #include "esp_system.h"
 const int wdtTimeout = 30*1000;  //time in ms to trigger the watchdog
@@ -59,7 +59,7 @@ uint8_t conf[] ={0xc0, 0x00, 0x08,
                 0x00, //CRYPT
                 0x00};
 struct  __attribute__((packed, aligned(4))) msgStruct{ 
-  char conf_0 = 0xFF;
+  char conf_0 = 0xFF;//GateWay adress 0x0000
   char conf_1 = 0xFF;
   char channel = 0x09;
   uint16_t myadress ;
@@ -78,6 +78,14 @@ void SwitchToNormalMode(void){
   delay(100);
 }
 
+  /**
+   * @brief WOR受信モード(M0=0,M1=1)へ移行する
+   */
+  void SwitchToWORReceivingMode(void){
+  digitalWrite(LoRa_ModeSettingPin_M0, 0);
+  digitalWrite(LoRa_ModeSettingPin_M1, 1);
+  delay(100);
+}
 
   /**
    * @brief コンフィグ/sleepモード(M0=1,M1=1)へ移行する
@@ -95,10 +103,10 @@ void onReceive(int len){
     SerialMon.printf("%02x ",buf[j]);
     j++;
   }
-    EEPROM.write(0, buf[0]);  
-    EEPROM.write(1, buf[1]);  
+    EEPROM.write(0, buf[0]);  //ADDH
+    EEPROM.write(1, buf[1]);  //ADDL
     EEPROM.commit();
-  senserID = buf[0]<<8|buf[1];
+  senserID = buf[0]<<8 | buf[1];
   SerialMon.printf("change sensorID: %d\n",senserID);
   return ;
 }
@@ -119,24 +127,55 @@ float getTemp(){
 }
 
 void IRAM_ATTR deep_sleep(){
-        SerialMon.printf("sleep \n");
-        SwitchToConfigurationMode();
-        // if (bootCount == 0){
-        //   delay(10000);
-        // } 
-        if (bootCount < 10)
-        {
-          sleepSec = 25;
-        }
-        bootCount++;
-        esp_sleep_enable_timer_wakeup(sleepSec * 1000 * 1000);
-        // gpio_hold_en(GPIO_NUM_20);
-        // gpio_deep_sleep_hold_en();
-      	esp_deep_sleep_start();
-}
+    SwitchToWORReceivingMode();
+		delay(100);
+    Serial.println();
+		if (ESP_OK == gpio_hold_en(LoRa_ModeSettingPin_M0)){
+			Serial.println("HOLD LoRa_ModeSettingPin_M0");
+		}else{
+			Serial.println("NO HOLD LoRa_ModeSettingPin_M0");
+		}
+		if (ESP_OK == gpio_hold_en(LoRa_ModeSettingPin_M1)){
+				Serial.println("HOLD LoRa_ModeSettingPin_M1");
+			}else{
+				Serial.println("NO HOLD 1LoRa_ModeSettingPin_M1");
+			}
+    if(esp_sleep_is_valid_wakeup_gpio(LoRa_AUXPin)){
+      esp_deep_sleep_enable_gpio_wakeup(BIT(4), ESP_GPIO_WAKEUP_GPIO_LOW);
+      gpio_deep_sleep_hold_en();
+      //Go to sleep now
+      Serial.println("Going to sleep now");
+      delay(1000);
+      esp_deep_sleep_start();
+    }else{
+      Serial.println("NG pin");
+    }
 
+}
+void wakeup_cause_print() {
+  esp_sleep_wakeup_cause_t wakeup_reason;
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  switch (wakeup_reason) {
+    case 0: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_UNDEFINED"); break;
+    case 1: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_ALL"); break;
+    case 2: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_EXT0"); break;
+    case 3: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_EXT1"); break;
+    case 4: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_TIMER"); break;
+    case 5: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_TOUCHPAD"); break;
+    case 6: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_ULP"); break;
+    case 7: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_GPIO"); break;
+    case 8: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_UART"); break;
+    case 9: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_WIFI"); break;
+    case 10: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_COCPU"); break;
+    case 11: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_COCPU_TRAP_TRIG"); break;
+    case 12: Serial.println("Wakeup caused by ESP_SLEEP_WAKEUP_BT"); break;
+    default: Serial.println("Wakeup was not caused by deep sleep"); break;
+  }
+}
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LoRa_ModeSettingPin_M0, OUTPUT);
+  pinMode(LoRa_ModeSettingPin_M1, OUTPUT);
   timer = timerBegin(0, 80, true);                  //timer 0, div 80
   timerAttachInterrupt(timer, &deep_sleep, true);  //attach callback
   timerAlarmWrite(timer, wdtTimeout * 1000, false); //set time in us
@@ -144,41 +183,31 @@ void setup() {
   timerWrite(timer, 0);
   SerialMon.begin(9600);
   delay(500);
-  // gpio_hold_dis(GPIO_NUM_20);
-  // gpio_deep_sleep_hold_dis();
-  // while(!SerialMon){}; // SerialMon init wait
-  SerialMon.println("start");
-  SerialMon.println(OWN_ADDRESS);
-  pinMode( SW_LOW ,INPUT_PULLUP);
-  pinMode( SW_HIGH ,INPUT_PULLUP);
-  pinMode( SW_COM ,OUTPUT);
-  digitalWrite ( SW_COM ,LOW);
-  // pinMode( I2C_VCC ,OUTPUT);
-  // digitalWrite ( I2C_VCC ,HIGH);
-  // pinMode( SENSOR_3V3 ,OUTPUT);
-  // pinMode( SENSOR_GND ,OUTPUT);
-  // digitalWrite ( SENSOR_GND ,LOW);
-  pinMode(LoRa_ModeSettingPin_M0, OUTPUT);
-  pinMode(LoRa_ModeSettingPin_M1, OUTPUT);
-  #ifdef SECOND_ADDRESS
-    pinMode( SECOND_SW_LOW ,INPUT_PULLUP);
-    pinMode( SECOND_SW_HIGH ,INPUT_PULLUP);
-    pinMode( SECOND_SW_COM ,OUTPUT);
-    digitalWrite ( SECOND_SW_COM ,LOW);
-  #endif
-  
-  // E220-900T22S(JP)へのLoRa初期設定
+    // E220-900T22S(JP)へのLoRa初期設定
   SerialLoRa.end(); // end()を実行　←←追加
   delay(1000); // 1秒待つ　 ←←追加
   SerialLoRa.begin(LoRa_BaudRate, SERIAL_8N1, LoRa_Tx_ESP_RxPin,LoRa_Rx_ESP_TxPin);
-
   EEPROM.begin(2);
-  if(!bootCount){
-    Wire.begin((uint8_t)I2C_DEV_ADDR, I2C_SDA, I2C_SCL, 100000);
-    Wire.onReceive(onReceive);
+  wakeup_cause_print();
+  wakeup_reason = esp_sleep_get_wakeup_cause();
+  if (ESP_SLEEP_WAKEUP_GPIO  == wakeup_reason) {
+    Serial.println("Waked up from external GPIO!");
+    gpio_hold_dis(LoRa_ModeSettingPin_M0);
+    gpio_hold_dis(LoRa_ModeSettingPin_M1);
+    gpio_deep_sleep_hold_dis();
+    SwitchToNormalMode();
+    senserID = OWN_ADDRESS; 
+    SerialMon.printf("\n sensorID: %d\n",senserID);
+    msg.myadress =  senserID;
+    delay(1000);
+  }else{
+    Serial.println("Waked up from nomal power on!");
+    // Wire.begin((uint8_t)I2C_DEV_ADDR, I2C_SDA, I2C_SCL, 100000);
+    // Wire.onReceive(onReceive);
     delay(20000);
 
-    senserID = (EEPROM.read(0) >> 8) | EEPROM.read(1); 
+    // senserID = (EEPROM.read(0) << 8) | EEPROM.read(1); 
+    senserID = OWN_ADDRESS; 
     SerialMon.printf("\n sensorID: %d\n",senserID);
     msg.myadress =  senserID;
     
@@ -194,13 +223,35 @@ void setup() {
 
     delay(100);
     while(!digitalRead(LoRa_AUXPin)){}
-
+    deep_sleep();
   }
+//	e32ttl.setMode(MODE_0_NORMAL);
+//	delay(1000);
+  Serial.println("Wake and start ");
+  pinMode( SW_LOW ,INPUT_PULLUP);
+  pinMode( SW_HIGH ,INPUT_PULLUP);
+  pinMode( SW_COM ,OUTPUT);
+  digitalWrite ( SW_COM ,LOW);
+  // pinMode( I2C_VCC ,OUTPUT);
+  // digitalWrite ( I2C_VCC ,HIGH);
+  // pinMode( SENSOR_3V3 ,OUTPUT);
+  // pinMode( SENSOR_GND ,OUTPUT);
+  // digitalWrite ( SENSOR_GND ,LOW);
+
+  #ifdef SECOND_ADDRESS
+    pinMode( SECOND_SW_LOW ,INPUT_PULLUP);
+    pinMode( SECOND_SW_HIGH ,INPUT_PULLUP);
+    pinMode( SECOND_SW_COM ,OUTPUT);
+    digitalWrite ( SECOND_SW_COM ,LOW);
+  #endif
+  
+
   SerialLoRa.flush();
   // ノーマルモード(M0=0,M1=0)へ移行する
   SwitchToNormalMode();
   while(!digitalRead(LoRa_AUXPin)){}
-  senserID = (EEPROM.read(0) >> 8) | EEPROM.read(1); 
+  delay(10000);
+  // senserID = (EEPROM.read(0) >> 8) | EEPROM.read(1); 
   SerialMon.printf("sensorID: %d\n",senserID);
   msg.myadress =  senserID;
   msg.temp = getTemp();

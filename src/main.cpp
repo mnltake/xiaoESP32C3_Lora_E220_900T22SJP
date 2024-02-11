9706752538bbbba843a2c2f6ad97aa4943fdfcaf
mnltake <minolu2003@ybb.ne.jp>
2024-02-11 19:06:15 +0900

    PCB I2C

--- src/main.cpp
+++ src/main.cpp
@@ -18,44 +18,33 @@
 
 // E220-900T22S(JP)のbaud rate
 #define LoRa_BaudRate 9600
-
-int16_t senserID = -1;
-#define OWN_ADDRESS 131
+RTC_DATA_ATTR int16_t senserID = -1;
+#define OWN_ADDRESS 160
 // #define SECOND_ADDRESS 305
 
-#define SW_LOW D8
-#define SW_HIGH D9
-// #define SW_COM D10
-#define I2C_VCC D3
+#define L1 D9
+#define H1 D10
 #define I2C_SDA D4
 #define I2C_SCL D5
-// #define SECOND_SW_LOW D3
-// #define SECOND_SW_HIGH D6
-// #define SECOND_SW_COM D5
-// #define SENSOR_GND D5
-// #define SENSOR_3V3 D6
-// #define SENSOR_DQ D3
-uint64_t sleepSec = 60*60 - 5;//実行時間5ｓ
+#define L2 D3
+#define H2 D8
+
+// uint64_t sleepSec = 60*60 - 5;//実行時間5ｓ
 RTC_DATA_ATTR uint16_t bootCount = 0;
+uint16_t waitmillsec = senserID*30 + bootCount;
 esp_sleep_source_t  wakeup_reason;
 //WDT
 #include "esp_system.h"
-const int wdtTimeout = 60*1000;  //time in ms to trigger the watchdog
+const int wdtTimeout = 60*1000;  //time in ms to trigger the watchdog sec
 hw_timer_t *timer = NULL;
 
-//DS18B20
-// #include <OneWire.h>
-// #include <DallasTemperature.h>
-// OneWire oneWire(SENSOR_DQ);
-// DallasTemperature sensors(&oneWire);
-
 uint8_t conf[] ={0xc0, 0x00, 0x08, 
-                OWN_ADDRESS >> 8, //ADDH
-                OWN_ADDRESS & 0xff, //ADDL
-                0b01110000, // baud_rate 9600 bps  SF:9 BW:125
+                senserID >> 8, //ADDH
+                senserID & 0xff, //ADDL
+                0b01110000, // baud_rate 115200 bps  SF:9 BW:125
                 0b11100000, //subpacket_size 32, rssi_ambient_noise_flag on, transmitting_power 13 dBm
                 0x09, //own_channel
-                0b10000101, //RSSI on ,fix mode,wor_cycle 2000 ms
+                0b11000111, //RSSI on ,fix mode,wor_cycle 4000 ms
                 0x00, //CRYPT
                 0x00};
 struct  __attribute__((packed, aligned(4))) msgStruct{ 
@@ -173,7 +162,6 @@ void wakeup_cause_print() {
   }
 }
 void setup() {
-  // put your setup code here, to run once:
   pinMode(LoRa_ModeSettingPin_M0, OUTPUT);
   pinMode(LoRa_ModeSettingPin_M1, OUTPUT);
   timer = timerBegin(0, 80, true);                  //timer 0, div 80
@@ -181,7 +169,7 @@ void setup() {
   timerAlarmWrite(timer, wdtTimeout * 1000, false); //set time in us
   timerAlarmEnable(timer);                          //enable interrupt
   timerWrite(timer, 0);
-  SerialMon.begin(9600);
+  SerialMon.begin(115200);
   delay(500);
     // E220-900T22S(JP)へのLoRa初期設定
   SerialLoRa.end(); // end()を実行　←←追加
@@ -195,8 +183,9 @@ void setup() {
     gpio_hold_dis(LoRa_ModeSettingPin_M0);
     gpio_hold_dis(LoRa_ModeSettingPin_M1);
     gpio_deep_sleep_hold_dis();
+    SwitchToConfigurationMode();//boot text clear
     SwitchToNormalMode();
-    senserID = OWN_ADDRESS; 
+    // senserID = OWN_ADDRESS; 
     SerialMon.printf("\n sensorID: %d\n",senserID);
     msg.myadress =  senserID;
     delay(100);
@@ -204,10 +193,10 @@ void setup() {
     Serial.println("Waked up from nomal power on!");
     Wire.begin((uint8_t)I2C_DEV_ADDR, I2C_SDA, I2C_SCL, 100000);
     Wire.onReceive(onReceive);
-    delay(10000);
+    delay(20000);
 
     senserID = (EEPROM.read(0) << 8) | EEPROM.read(1); 
-    senserID = OWN_ADDRESS; 
+    // senserID = OWN_ADDRESS; 
     SerialMon.printf("\n sensorID: %d\n",senserID);
     msg.myadress =  senserID;
     
@@ -225,32 +214,63 @@ void setup() {
     while(!digitalRead(LoRa_AUXPin)){}
     deep_sleep();
   }
-//	e32ttl.setMode(MODE_0_NORMAL);
+
 //	delay(1000);
   Serial.println("Wake and start ");
-  pinMode( SW_LOW ,INPUT_PULLUP);
-  pinMode( SW_HIGH ,INPUT_PULLUP);
-  pinMode( SW_COM ,OUTPUT);
-  digitalWrite ( SW_COM ,LOW);
-  // pinMode( I2C_VCC ,OUTPUT);
-  // digitalWrite ( I2C_VCC ,HIGH);
-  // pinMode( SENSOR_3V3 ,OUTPUT);
-  // pinMode( SENSOR_GND ,OUTPUT);
-  // digitalWrite ( SENSOR_GND ,LOW);
+  pinMode( L1 ,INPUT_PULLUP);
+  pinMode( H1 ,INPUT_PULLUP);
 
   #ifdef SECOND_ADDRESS
-    pinMode( SECOND_SW_LOW ,INPUT_PULLUP);
-    pinMode( SECOND_SW_HIGH ,INPUT_PULLUP);
-    pinMode( SECOND_SW_COM ,OUTPUT);
-    digitalWrite ( SECOND_SW_COM ,LOW);
+    pinMode( L2 ,INPUT_PULLUP);
+    pinMode( H2 ,INPUT_PULLUP);
   #endif
   
 
+  // SerialLoRa.flush();
+  // ノーマルモード(M0=0,M1=0)へ移行する
+  // SwitchToNormalMode();
+  // while(!digitalRead(LoRa_AUXPin)){}
+  // delay(1000);
+  // SerialLoRa.write(0x00);
+  // SerialLoRa.flush();
+  #ifdef SECOND_ADDRESS
+    timerWrite(timer, 0);
+    delay(5000);
+    timerWrite(timer, 0);
+    msg.myadress = SECOND_ADDRESS;
+    msg.temp = getTemp();
+    // msg.temp = -127;
+    msg.water = digitalRead( L2) * 49 + digitalRead( H2) * 51; //ここに水位
+    msg.bootcount = bootCount;
+    SerialMon.println(SECOND_ADDRESS);
+    SerialMon.printf("boot:%d \nWater:%d \nTemp:%f\n" ,msg.bootcount,msg.water,msg.temp);
+    SerialLoRa.flush();
+    uint8_t payload2[]={msg.targetAdressH, msg.targetAdressL, msg.targetChannel ,
+
+                      msg.myadress & 0xff ,msg.myadress >> 8 ,
+                      msg.water &0xff, 0x00, 
+                      msg.bootcount & 0xff, msg.bootcount >> 8, 
+                      0x00, 0x00 ,0xfe, 0xc2, 0x00, 0x00};
+      SerialMon.printf("I send data\n\n");
+      for (size_t i = 0; i < sizeof(payload2); i++)
+    {
+
+    SerialMon.printf(" %02x",payload2[i]);
+    }
+    SerialMon.println();
+    SerialLoRa.write((uint8_t *)&payload2, sizeof(payload2));
+    SerialLoRa.flush();
+    delay(100);
+  #endif
+
+  delay(500);
   SerialLoRa.flush();
   // ノーマルモード(M0=0,M1=0)へ移行する
   SwitchToNormalMode();
   while(!digitalRead(LoRa_AUXPin)){}
-  delay(3000);
+
+  delay(waitmillsec);//他と重ならない秒数
+  SerialMon.printf("waitmillsec: %d\n",waitmillsec);
   senserID = (EEPROM.read(0) >> 8) | EEPROM.read(1); 
   SerialMon.printf("sensorID: %d\n",senserID);
   msg.myadress =  senserID ;
@@ -258,7 +278,7 @@ void setup() {
   byte temperatureByteData[sizeof(float)];
   memcpy(temperatureByteData, &msg.temp, sizeof(float));
   // msg.temp = -127;
-  msg.water = digitalRead( SW_LOW) * 49 + digitalRead( SW_HIGH) * 51; //ここに水位
+  msg.water = digitalRead( L1 ) * 49 + digitalRead( H1 ) * 51; //ここに水位
   msg.bootcount = bootCount;
   Serial.printf("boot:%d \nWater:%d \nTemp:%f\n" ,msg.bootcount,msg.water,msg.temp);
   SerialLoRa.flush();
